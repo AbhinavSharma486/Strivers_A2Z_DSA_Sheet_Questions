3270b2dfd5394e82d26a18079a8ef262a08803e9
Abhinav Sharma <114858652+AbhinavSharma486@users.noreply.github.com>
2025-11-03 17:38:33 +0530

    Day 2

---
+++ A2Z Strivers DSA Sheet/Step 1 Learn the basics/Lec 2 Build up logical thinking/file.cpp
@@ -0,0 +1,163 @@
+// & Pattern 1 :-
+// * * * * *
+// * * * * *
+// * * * * *
+// * * * * *
+// * * * * *
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//   string s = "* ";
+
+//   for (int i = 0; i < 5; i++)
+//   {
+//     for (int j = 0; j < 5; j++)
+//     {
+//       cout << s;
+//     }
+//     cout << endl;
+//   }
+// }
+
+// & Pattern 2 :-
+// *
+// * *
+// * * *
+// * * * *
+// * * * * *
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//   for (int i = 0; i < 5; i++)
+//   {
+//     for (int j = 0; j <= i; j++)
+//     {
+//       cout << "* ";
+//     }
+//     cout << endl;
+//   }
+// }
+
+// & Pattern 3 :-
+// * 1
+// * 1 2
+// * 1 2 3
+// * 1 2 3 4
+// * 1 2 3 4 5
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//   for (int i = 1; i <= 5; i++)
+//   {
+//     for (int j = 1; j <= i; j++)
+//     {
+//       cout << j << " ";
+//     }
+//     cout << endl;
+//   }
+// }
+
+// & Pattern 4 :-
+// * 1
+// * 2 2
+// * 3 3 3
+// * 4 4 4 4
+// * 5 5 5 5 5
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//   for (int i = 1; i <= 5; i++)
+//   {
+//     for (int j = 1; j <= i; j++)
+//     {
+//       cout << i << " ";
+//     }
+//     cout << endl;
+//   }
+// }
+
+// & Pattern 5 :-
+// * * * * *
+// * * * *
+// * * *
+// * *
+// *
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//   for (int i = 0; i < 5; i++)
+//   {
+//     for (int j = 0; j < 5 - i; j++)
+//     {
+//       cout << "* ";
+//     }
+//     cout << endl;
+//   }
+// }
+
+// & Pattern 6 :-
+// * 1 2 3 4 5
+// * 1 2 3 4
+// * 1 2 3
+// * 1 2
+// * 1
+
+// #include <iostream>
+// using namespace std;
+
+// int printTriangle(int n)
+// {
+//   for (int i = n; i >= 1; i--)
+//   {
+//     for (int j = 1; j <= i; j++)
+//     {
+//       cout << j << " ";
+//     }
+//     cout << endl;
+//   }
+//   return 0;
+// }
+
+// int main()
+// {
+//   int n;
+//   cout << "Enter value of n: ";
+//   cin >> n; // input actually matters now
+//   printTriangle(n);
+//   return 0;
+// }
+
+// & Pattern 7 :-
+//         *
+//       * * *
+//     * * * * *
+//   * * * * * * *
+// * * * * * * * * *
+
+#include <iostream>
+using namespace std;
+int main()
+{
+  for (int i = 1; i <= 5; i++)
+  {
+    for (int j = 1; j <= 5 - i; j++)
+    {
+      cout << "  ";
+    }
+    for (int k = 1; k <= i + (i - 1); k++)
+    {
+      cout << "* ";
+    }
+    cout << endl;
+  }
+}
