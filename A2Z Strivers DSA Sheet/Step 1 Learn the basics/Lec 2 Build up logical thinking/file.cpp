792191d0fa2a0d0e413a5abf87957a018f06b29b
Abhinav Sharma <114858652+AbhinavSharma486@users.noreply.github.com>
2025-11-04 23:05:33 +0530

    Day 3 Add new patterns

--- A2Z Strivers DSA Sheet/Step 1 Learn the basics/Lec 2 Build up logical thinking/file.cpp
+++ A2Z Strivers DSA Sheet/Step 1 Learn the basics/Lec 2 Build up logical thinking/file.cpp
@@ -144,20 +144,138 @@
 //   * * * * * * *
 // * * * * * * * * *
 
-#include <iostream>
-using namespace std;
-int main()
-{
-  for (int i = 1; i <= 5; i++)
-  {
-    for (int j = 1; j <= 5 - i; j++)
-    {
-      cout << "  ";
-    }
-    for (int k = 1; k <= i + (i - 1); k++)
-    {
-      cout << "* ";
-    }
-    cout << endl;
-  }
-}
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//   for (int i = 1; i <= 5; i++)
+//   {
+//     for (int j = 1; j <= 5 - i; j++)
+//     {
+//       cout << "  ";
+//     }
+//     for (int k = 1; k <= 2 * i - 1; k++)
+//     {
+//       cout << "* ";
+//     }
+//     cout << endl;
+//   }
+// }
+
+// & Pattern 8 :-
+// * * * * * * * * *
+//   * * * * * * *
+//     * * * * *
+//       * * *
+//         *
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//   int n;
+//   cout << "Enter value of n : ";
+//   cin >> n;
+
+//   for (int i = n; i >= 1; i--)
+//   {
+//     for (int j = n - i; j >= 1; j--)
+//     {
+//       cout << "  ";
+//     }
+
+//     for (int k = 1; k <= 2 * i - 1; k++)
+//     {
+//       cout << "* ";
+//     }
+//     cout << endl;
+//   }
+// }
+
+// & Pattern 9 :-
+//         *
+//       * * *
+//     * * * * *
+//   * * * * * * *
+// * * * * * * * * *
+// * * * * * * * * *
+//   * * * * * * *
+//     * * * * *
+//       * * *
+//         *
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//   int n;
+//   cout << "Enter value of n : ";
+//   cin >> n;
+
+//   for (int i = 1; i <= n; i++)
+//   {
+//     for (int j = 1; j <= n - i; j++)
+//     {
+//       cout << "  ";
+//     }
+
+//     for (int k = 1; k <= 2 * i - 1; k++)
+//     {
+//       cout << "* ";
+//     }
+//     cout << endl;
+//   }
+
+//   for (int i = n; i >= 1; i--)
+//   {
+//     for (int j = n - i; j >= 1; j--)
+//     {
+//       cout << "  ";
+//     }
+
+//     for (int k = 1; k <= 2 * i - 1; k++)
+//     {
+//       cout << "* ";
+//     }
+//     cout << endl;
+//   }
+// }
+
+// & Pattern 10 :-
+//*
+//* *
+//* * *
+//* * * *
+//* * * * *
+//* * * *
+//* * *
+//* *
+//*
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//   int n;
+//   cout << "Enter value of n : ";
+//   cin >> n;
+
+//   for (int i = 1; i <= n; i++)
+//   {
+//     for (int j = 1; j <= i; j++)
+//     {
+//       cout << "* ";
+//     }
+//     cout << endl;
+//   }
+
+//   for (int i = n - 1; i >= 1; i--)
+//   {
+//     for (int j = i; j >= 1; j--)
+//     {
+//       cout << "* ";
+//     }
+//     cout << endl;
+//   }
+// }
+
