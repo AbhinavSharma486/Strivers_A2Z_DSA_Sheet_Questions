3cd29fbd3c2431e3bac52fb9eaca26623c4f2402
Abhinav Sharma <114858652+AbhinavSharma486@users.noreply.github.com>
2025-11-05 19:39:50 +0530

    Add new patterns 11 to 16 with corresponding implementations

--- A2Z Strivers DSA Sheet/Step 1 Learn the basics/Lec 2 Build up logical thinking/file.cpp
+++ A2Z Strivers DSA Sheet/Step 1 Learn the basics/Lec 2 Build up logical thinking/file.cpp
@@ -279,3 +279,182 @@
 //   }
 // }
 
+// & Pattern 11 :-
+// * 1
+// * 0 1
+// * 1 0 1
+// * 0 1 0 1
+// * 1 0 1 0 1
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//   int n;
+//   cout << "Enter value of n : ";
+//   cin >> n;
+
+//   int start = 0;
+
+//   for (int i = 1; i <= n; i++)
+//   {
+
+//     if (i % 2 == 0)
+//       start = 0;
+//     else
+//       start = 1;
+
+//     for (int j = 1; j <= i; j++)
+//     {
+//       cout << start << " ";
+//       start = 1 - start;
+//     }
+//     cout << endl;
+//   }
+// }
+
+// & Pattern 12 :-
+// * 1             1
+// * 1 2         2 1
+// * 1 2 3     3 2 1
+// * 1 2 3 4 4 3 2 1
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//   int n;
+//   cout << "Enter value of n : ";
+//   cin >> n;
+
+//   for (int i = 1; i <= n; i++)
+//   {
+//     for (int j = 1; j <= i; j++)
+//     {
+//       cout << j << " ";
+//     }
+
+//     for (int k = 1; k <= 2 * (n - i); k++)
+//     {
+//       cout << "  ";
+//     }
+
+//     for (int m = i; m >= 1; m--)
+//     {
+//       cout << m << " ";
+//     }
+//     cout << endl;
+//   }
+// }
+
+// & Pattern 13 :-
+// * 1
+// * 2 3
+// * 4 5 6
+// * 7 8 9 10
+// * 11 12 13 14 15
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//   int n;
+//   cout << "Enter value of n : ";
+//   cin >> n;
+
+//   int count = 1;
+
+//   for (int i = 1; i <= n; i++)
+//   {
+
+//     for (int j = 1; j <= i; j++)
+//     {
+//       cout << count << " ";
+//       count++;
+//     }
+
+//     cout << endl;
+//   }
+// }
+
+// & Pattern 14 :-
+// * A
+// * A B
+// * A B C
+// * A B C D
+// * A B C D E
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//   int n;
+//   cout << "Enter value of n : ";
+//   cin >> n;
+
+//   for (int i = 1; i <= n; i++)
+//   {
+//     char ch = 'A';
+//     for (int j = 1; j <= i; j++)
+//     {
+//       cout << ch << " ";
+//       ch++;
+//     }
+//     cout << endl;
+//   }
+// }
+
+// & Pattern 15 :-
+// * A B C D E
+// * A B C D
+// * A B C
+// * A B
+// * A
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//   int n;
+//   cout << "Enter value of n : ";
+//   cin >> n;
+
+//   for (int i = n; i >= 1; i--)
+//   {
+//     char ch = 'A';
+
+//     for (int j = 1; j <= i; j++)
+//     {
+//       cout << ch << " ";
+//       ch++;
+//     }
+//     cout << endl;
+//   }
+// }
+
+// & Pattern 16 :-
+// * A
+// * B B
+// * C C C
+// * D D D D
+// * E E E E E
+
+#include <iostream>
+using namespace std;
+int main()
+{
+  int n;
+  cout << "Enter value of n : ";
+  cin >> n;
+
+  for (int i = 1; i <= n; i++)
+  {
+    char ch = 'A' + i - 1;
+
+    for (int j = 1; j <= i; j++)
+    {
+      cout << ch << " ";
+    }
+    cout << endl;
+  }
+}
